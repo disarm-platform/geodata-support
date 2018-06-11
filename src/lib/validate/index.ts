fd6696c038d8799c44e5b2e3d7486a094c21382f
Jonathan Smith <jonathan@peoplesized.com>
2018-06-11 16:08:38 +0200

    not sure we can/should do anything with schema

--- src/lib/validate/index.ts
+++ src/lib/validate/index.ts
@@ -1,3 +1,2 @@
 export { validate_geodata } from './validate_geodata';
 export { validate_schema } from './validate_schema';
-export { validate_spatial_hierarchy } from './validate_spatial_hierarchy';

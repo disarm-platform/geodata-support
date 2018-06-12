378c648960c79a2a56f04e01b976f2c3ce842936
Jonathan Smith <jonathan@peoplesized.com>
2018-06-12 20:04:12 +0200

    Better typing

--- src/lib/validate_spatial_hierarchy/check_layers_and_levels.ts
+++ src/lib/validate_spatial_hierarchy/check_layers_and_levels.ts
@@ -2,20 +2,20 @@ import { difference } from 'lodash';
 import { EValidationStatus, TValidationResponse } from '../../config_types/TValidationResponse';
 
 export function check_layers_and_levels(geodata_layer_names, sh_level_names): TValidationResponse {
-  const missing_from_geodata   = difference(sh_level_names, geodata_layer_names);
-  const missing_from_sh_levels = difference(geodata_layer_names, sh_level_names);
+  const missing_from_geodata   = difference<string>(sh_level_names, geodata_layer_names);
+  const missing_from_sh_levels = difference<string>(geodata_layer_names, sh_level_names);
 
   if (missing_from_geodata.length > 0) {
     return {
       message: 'Missing fields from geodata levels',
       status: EValidationStatus.Red,
-      support_messages: missing_from_geodata as string[]
+      support_messages: missing_from_geodata
     };
   } else if (missing_from_sh_levels.length > 0) {
     return {
       message: 'Missing fields from spatial_hierarchy levels',
       status: EValidationStatus.Red,
-      support_messages: missing_from_sh_levels as string[]
+      support_messages: missing_from_sh_levels 
     };
   } else if (missing_from_geodata.length === 0 && missing_from_sh_levels.length === 0) {
     return {

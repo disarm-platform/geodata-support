29690553e86aa8c8e80ffdbaee7980a14aa058a2
Jonathan Smith <jonathan@peoplesized.com>
2018-06-12 11:38:22 +0200

    no fields, no problem

--- src/lib/summarise/summarise.spec.ts
+++ src/lib/summarise/summarise.spec.ts
@@ -1,18 +1,24 @@
 // tslint:disable:no-expression-statement
-import test from 'ava'
+import test from 'ava';
 import { unique_fields } from './summarise';
 
 test('simple unique fields', t => {
-  const properties_array = [{id: 1}, {id: 2}]
-  const actual = unique_fields(properties_array)
-  const expected = ['id']
-  t.deepEqual(actual, expected)
-})
+  const properties_array = [{ id: 1 }, { id: 2 }];
+  const actual = unique_fields(properties_array);
+  const expected = ['id'];
+  t.deepEqual(actual, expected);
+});
 
 test('more fields unique fields', t => {
-  const properties_array = [{id: 1, egg: 2}, {id: 2, face: 3}]
-  const actual = unique_fields(properties_array)
-  const expected = ['id', 'egg', 'face']
-  t.deepEqual(actual, expected)
-})
+  const properties_array = [{ id: 1, egg: 2 }, { id: 2, face: 3 }];
+  const actual = unique_fields(properties_array);
+  const expected = ['id', 'egg', 'face'];
+  t.deepEqual(actual, expected);
+});
 
+test('no fields', t => {
+  const properties_array = [{}, {}];
+  const actual = unique_fields(properties_array);
+  const expected = [];
+  t.deepEqual(actual, expected);
+});

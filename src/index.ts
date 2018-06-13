0783d5593c66c15c685596d32d5d9afe0c072f47
Jonathan Smith <jonathan@peoplesized.com>
2018-06-13 10:55:28 +0200

    order according to real-life use

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { summarise } from './lib/summarise';
 export { validate_layer_schema } from './lib/validate_geodata';
+export { summarise } from './lib/summarise';
 export { validate_spatial_hierarchy } from './lib/validate_spatial_hierarchy';

dab01de6b6da6c42cc61fac2047b4ff5b58e1036
Jonathan Smith <jonathan@peoplesized.com>
2018-06-13 10:55:14 +0200

    improve doc

--- src/lib/summarise/summarise.ts
+++ src/lib/summarise/summarise.ts
@@ -14,7 +14,7 @@ export enum EFieldType {
 
 export interface TFieldSummary {
   field_name: string;
-  exists_on_all: boolean;
+  exists_on_all: boolean; // The field exists on every feature
   type: EFieldType; // Checks only those values given, so use with exists_on_all
   unique: boolean; // Checks only those values given, so use with exists_on_all
   sample_values?: string[]; // Optional sample of the values for the field

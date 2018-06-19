2d59cef96ea71c8fc33dcd73c263303cdeb749c1
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-19 13:41:42 +0200

    Export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { validate_layer_schema } from './lib/validate_geodata';
 export { summarise } from './lib/summarise';
 export { validate_spatial_hierarchy } from './lib/validate_spatial_hierarchy';
+export { generate_location_selection } from './lib/generate_location_selection';

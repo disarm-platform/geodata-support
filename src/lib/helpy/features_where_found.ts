00d152f2772de2e849da70152e7b82b0a8d48b04
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-19 09:04:11 +0200

    Annotate as returning array

--- src/lib/helpy/features_where_found.ts
+++ src/lib/helpy/features_where_found.ts
@@ -1,5 +1,5 @@
 import { has } from 'lodash';
 
-export function features_where_found(field_name, properties_array) {
+export function features_where_found(field_name, properties_array): any[] {
   return properties_array.filter(properties => has(properties, field_name));
 }

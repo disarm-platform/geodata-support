ccd1cf1899c29001752b28f6f56aad2edfe82fc7
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-19 13:41:38 +0200

    Return validation error if geodata or sh is not valid

--- src/lib/generate_location_selection/generate_location_selection.ts
+++ src/lib/generate_location_selection/generate_location_selection.ts
@@ -1,16 +1,23 @@
 import { TSpatialHierarchy, TLevel } from "../../config_types/TSpatialHierarchy";
 import { TGeodata, TGeodataLayer } from "../../config_types/TGeodata";
 import { TLocationSelection, TLocationSelectionOption } from "../../config_types/TLocationSelection";
+import { validate_spatial_hierarchy } from "../validate_spatial_hierarchy";
+import { EValidationStatus, TValidationResponse } from "../../config_types/TValidationResponse";
 
 
-export function generate_location_selection(spatial_hierarchy: TSpatialHierarchy, geodata: TGeodata): TLocationSelection {
+export function generate_location_selection(spatial_hierarchy: TSpatialHierarchy, geodata: TGeodata): TLocationSelection | TValidationResponse {
+  const validation_result = validate_spatial_hierarchy(spatial_hierarchy, geodata)
+  if (validation_result.status === EValidationStatus.Red) {
+    return validation_result
+  }
+
   const location_selection: TLocationSelection = {}
 
   for (const level of spatial_hierarchy.levels) {
     const geodata_layer = geodata[level.name]
-    // tslint:disable
+    // tslint:disable:no-expression-statement
+    // tslint:disable:no-object-mutation
     location_selection[level.name] = generate_location_selection_for_level(geodata_layer, level)
-    // tslint:enable
   }
 
 

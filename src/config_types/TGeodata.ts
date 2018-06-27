15a07f8460dfdf7659c87ff27f79dd36f59f3a28
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-27 12:50:39 +0200

    Add GeodataLayerDefinitionType, will be used in registry-client

--- src/config_types/TGeodata.ts
+++ src/config_types/TGeodata.ts
@@ -1,7 +1,18 @@
 import { GeoJsonFeatureCollection } from './TGeoJSON';
+import { EValidationStatus } from './TValidationResponse';
+import { TFieldSummary } from './TGeodataSummary';
 
 export interface TGeodata {
   [k: string]: TGeodataLayer
+}
+
+// might want to rename it, would like it to just be TGeodataLayer, but then TGeodataLayer needs to be renamed
+// not used inside this lib, only externally
+export interface TGeodataLayerDefinition {
+  name: string;
+  file_name: string;
+  validation_status: EValidationStatus;
+  field_summary: TFieldSummary[];
 }
 
 /**
